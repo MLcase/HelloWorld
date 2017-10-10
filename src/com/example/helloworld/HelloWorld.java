98516488c3689a4e8f25187dd1a4d1daf5b20c21
Martin Lange <martinlange@Admins-iMac.fritz.box>
2017-10-10 11:27:38 +0200

    added comments and fibonacci sequence

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -6,14 +6,28 @@ public class HelloWorld {
         String option2 = "Hello World!";
         String message;
 
+        // randomizer selects one of the two messages
         if (Math.random() > 0.5) {
             message = option1;
         } else {
             message = option2;
         }
 
+        // output of selected message and its length
         System.out.println(message);
         System.out.println("This string has " + message.length() + " characters.");
 
+        // generates and outputs the first 10 numbers of the fibonacci sequence
+        int fibo = 1;
+        int prev = 0;
+        int help;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(fibo + " ");
+
+            help = fibo;
+            fibo = fibo+prev;
+            prev = help;
+        }
     }
 }
