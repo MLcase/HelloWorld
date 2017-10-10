fcae3792cbf7352a11f79114f4cd693afa13f4d3
Martin Lange <martinlange@Admins-iMac.fritz.box>
2017-10-10 10:47:05 +0200

    Added randomizer that selects between two messages

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,9 +2,18 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String message = "Oh, hello World!";
+        String option1 = "Oh, hello World!";
+        String option2 = "Hello World!";
+        String message;
+
+        if (Math.random() > 0.5) {
+            message = option1;
+        } else {
+            message = option2;
+        }
 
         System.out.println(message);
         System.out.println("This string has " + message.length() + " characters.");
+
     }
 }
