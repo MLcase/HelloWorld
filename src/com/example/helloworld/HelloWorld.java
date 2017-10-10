ca65e826ef5b3f3ba4eaeb5f23db984e12227217
Martin Lange <martinlange@Admins-iMac.fritz.box>
2017-10-10 10:04:53 +0200

    First version of HelloWorld

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "Hello World!";
+
+        System.out.println(message);
+        System.out.println("This string has " + message.length() + " characters.");
+    }
+}
