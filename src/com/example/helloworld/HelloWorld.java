304ab4f745a0ebfbc8502666bb347bc5b1449837
Stephan Doerfel <s.doerfel@micromata.de>
2017-10-10 10:21:54 +0200

    Change message

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String message = "Hello World!";
+        String message = "Oh, hello World!";
 
         System.out.println(message);
         System.out.println("This string has " + message.length() + " characters.");
